5dad79dbeb5867acd0f136057e84d72ecb3d1874
Daniel Upchurch <danielupchurch93@gmail.com>
2021-01-01 10:32:54 -0500

    Setting up user authentication. On login/register page

---
+++ db/seeders/20210101152248-add_users.js
@@ -0,0 +1,25 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      */
+      return queryInterface.bulkInsert('Users', [
+        // { username: "demoUser", email: "demoUser@demo.com", hashedPassword: ""}
+      ], {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('People', null, {});
+    */
+  }
+};
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,16 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const {asyncHandler, csrfProtection} = require('../utils');
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+//add a new user
+router.post('/users', csrfProtection, asyncHandler(async (req, res) => {
+
+}));
 
 module.exports = router;
