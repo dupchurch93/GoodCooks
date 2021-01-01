768e49019c78034d9fc08042a545a35fb83c8cdf
brandon <bparsons17@liberty.edu>
2021-01-01 11:09:01 -0500

    adding validations

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,39 @@ const express = require('express');
 const router = express.Router();
 const { asyncHandler, csrfProtection } = require('../utils');
 const db = require('../db/models');
+const { check, validationResult } = require('express-validator')
+const bcrypt = require('bcryptjs')
+
+const userValidator = [
+  check("username")
+    .exists({ checkFalsy: true })
+    .withMessage("Please provide a username")
+    .isLength({ max: 50 })
+    .withMessage("Username cannot be longer than 50 characters")
+    .custom((value) => {
+      return db.User.findOne({
+        where: { username: value },
+      }).then((user) => {
+        if (user) {
+          return Promise.reject("Username already exists.");
+        }
+      });
+    }),
+  check("email")
+    .exists({ checkFalsy: true })
+    .withMessage("Please provide an email")
+    .isLength({ max: 255 })
+    .withMessage("Email cannot be longer than 255 characters")
+    .custom((value) => {
+      return db.User.findOne({
+        where: { email: value },
+      }).then((email) => {
+        if (email) {
+          return Promise.reject("Email already exists.");
+        }
+      });
+    }),
+];
 
 /* GET user registration form */
 router.get('/register', csrfProtection, function (req, res, next) {
@@ -14,10 +47,10 @@ router.get('/register', csrfProtection, function (req, res, next) {
 });
 
 //add a new user
-router.post(
-  '/register',
-  csrfProtection,
-  asyncHandler(async (req, res) => {})
+router.post('/register',csrfProtection,asyncHandler(async (req, res) => {
+  const { username, emailAdress, password } = req.body
+
+})
 );
 
 //login an existing user
