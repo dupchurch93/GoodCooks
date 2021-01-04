4e1164580f8e2544797e95f10d307938364ddc09
Johnny <thejohnnytran27@gmail.com>
2021-01-04 14:40:57 -0600

    finish auth cycle

--- auth.js
+++ auth.js
@@ -1,36 +1,49 @@
 const db = require('./db/models');
 
 const loginUser = (req, res, user) => {
-    //saves the user to the session and sets the user id to the primary user key of the database
-    req.session.auth = {
-        userId: user.id
-    };
-}
+  //saves the user to the session and sets the user id to the primary user key of the database
+  req.session.auth = {
+    userId: user.id,
+  };
+};
 
 const restoreUser = async (req, res, next) => {
-    //called everytime we go to a new page/path, and checks if the session has the user authentication
-    if (req.session.auth) {
-        //grabs the userId from the saved session
-        const { userId } = req.session.auth;
-        //looks for the user in the database and sets the authentication to true if the database finds the user, otherwise throws an error
-        try {
-            const user = await db.User.findByPk(userId);
-            if(user) {
-                res.locals.authenticated=true;
-                res.locals.user = user;
-                next();
-            }
-        } catch (err) {
-            res.locals.authenticated = false;
-            next(err);
-        }
-    } else {
-        res.locals.authenticated = false;
+  //called everytime we go to a new page/path, and checks if the session has the user authentication
+  if (req.session.auth) {
+    //grabs the userId from the saved session
+    const { userId } = req.session.auth;
+    //looks for the user in the database and sets the authentication to true if the database finds the user, otherwise throws an error
+    try {
+      const user = await db.User.findByPk(userId);
+      if (user) {
+        res.locals.authenticated = true;
+        res.locals.user = user;
         next();
+      }
+    } catch (err) {
+      res.locals.authenticated = false;
+      next(err);
     }
-}
+  } else {
+    res.locals.authenticated = false;
+    next();
+  }
+};
+
+const logoutUser = (req, res) => {
+  delete req.session.auth;
+};
+
+const requireAuth = (req, res, next) => {
+  if (!res.locals.authenticated) {
+    return res.redirect('/users/login');
+  }
+  return next();
+};
 
 module.exports = {
-    loginUser,
-    restoreUser
+  loginUser,
+  restoreUser,
+  logoutUser,
+  requireAuth,
 };
--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,7 @@ const { asyncHandler, csrfProtection, userValidator, loginValidator } = require(
 const { validationResult } = require('express-validator');
 const { User } = require('../db/models/');
 const bcrypt = require('bcryptjs');
-const { loginUser } = require('../auth');
+const { loginUser, logoutUser } = require('../auth');
 
 /* GET user registration form */
 router.get('/register', csrfProtection, function (req, res, next) {
@@ -17,7 +17,11 @@ router.get('/register', csrfProtection, function (req, res, next) {
 });
 
 //add a new user
-router.post('/register',csrfProtection,userValidator,asyncHandler(async (req, res) => {
+router.post(
+  '/register',
+  csrfProtection,
+  userValidator,
+  asyncHandler(async (req, res) => {
     console.log('req.body', req.body);
     const { username, email, password } = req.body;
     const user = User.build({ username, email });
@@ -28,6 +32,7 @@ router.post('/register',csrfProtection,userValidator,asyncHandler(async (req, re
       const hashedPassword = await bcrypt.hash(password, 10);
       user.hashedPassword = hashedPassword;
       await user.save();
+      loginUser(req, res, user);
       res.redirect('/');
     } else {
       const errors = validatorErrors.array().map((error) => error.msg);
@@ -42,39 +47,53 @@ router.post('/register',csrfProtection,userValidator,asyncHandler(async (req, re
 );
 
 //login an existing user
-router.get('/login', csrfProtection, asyncHandler(async (req, res) => {
-  res.render('user-login', {
-    title: 'login',
-    csrfToken: req.csrfToken()
-  })
-  // console.log(user);
-}))
-
-router.post('/login',csrfProtection,loginValidator, asyncHandler(async(req, res)=> {
-  const { email, password } = req.body
-  const errors = []
-
-  const validatorErrors = validationResult(req);
-  if(validatorErrors.isEmpty){
-    const user = await User.findOne({ where : { email }});
-    if(user !== null){
-      const passwordMatch = await bcrypt.compare(password, user.hashedPassword.toString());
-      if(passwordMatch){
-        loginUser(req, res, user);
-        res.redirect('/');
-      }
-    } else {
-      errors.push("Login failed for the provided email address. Please try again");
-    }
-  } else {
-    errors = validatorErrors.array.map((error) => error.msg);
+router.get(
+  '/login',
+  csrfProtection,
+  asyncHandler(async (req, res) => {
     res.render('user-login', {
-      title: 'Login',
-      email,
-      errors,
+      title: 'login',
       csrfToken: req.csrfToken(),
     });
-  }
-}))
+    // console.log(user);
+  })
+);
+
+router.post(
+  '/login',
+  csrfProtection,
+  loginValidator,
+  asyncHandler(async (req, res) => {
+    const { email, password } = req.body;
+    const errors = [];
+
+    const validatorErrors = validationResult(req);
+    if (validatorErrors.isEmpty) {
+      const user = await User.findOne({ where: { email } });
+      if (user !== null) {
+        const passwordMatch = await bcrypt.compare(password, user.hashedPassword.toString());
+        if (passwordMatch) {
+          loginUser(req, res, user);
+          res.redirect('/');
+        }
+      } else {
+        errors.push('Login failed for the provided email address. Please try again');
+      }
+    } else {
+      errors = validatorErrors.array.map((error) => error.msg);
+      res.render('user-login', {
+        title: 'Login',
+        email,
+        errors,
+        csrfToken: req.csrfToken(),
+      });
+    }
+  })
+);
+
+router.post('/logout', (req, res) => {
+  logoutUser(req, res);
+  res.redirect('/');
+});
 
 module.exports = router;
