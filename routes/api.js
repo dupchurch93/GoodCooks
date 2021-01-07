4013267cf8e9d229715765becb596c19b1d6943c
Daniel Upchurch <danielupchurch93@gmail.com>
2021-01-07 13:04:43 -0500

    Save and Unsave feature working perfectly

--- routes/api.js
+++ routes/api.js
@@ -3,36 +3,33 @@ const router = express.Router();
 const { asyncHandler } = require('../utils');
 const { User, Recipe, sequelize, Cupboard_Recipe } = require('../db/models/');
 
-
 router.post(
   '/recipes/saveRecipe',
   asyncHandler(async (req, res) => {
     const { recipeId } = req.body;
     const { cupboardId } = req.body;
-    console.log(req.body)
     const recordsCreated = await Cupboard_Recipe.create({
       recipeId,
       cupboardId,
       cooked: false,
       favorited: false
     });
-    console.log('recordsCreated: ', recordsCreated)
     res.json({ recordsCreated });
   })
 );
 
-// router.post(
-//   '/recipes/:recipeId(//d+)/cupboards/:cupboardId(//d+)',
-//   asyncHandler(async (req, res) => {
-//     const recipeId = req.params.recipeId;
-//     const cupboardId = req.params.cupboardId;
-//     console.log(recipeId, cupboardId)
-//     const recordsCreated = await Cupboard_Recipe.create({
-//       recipeId,
-//       cupboardId,
-//     });
-//     res.json({ recordsCreated });
-//   })
-// );
+router.delete(
+  '/recipes/unsaveRecipe',
+  asyncHandler(async (req, res) => {
+    const { recipeId } = req.body;
+    const { cupboardId } = req.body;
+    const recordsDestroyed = await Cupboard_Recipe.destroy({
+      where: {
+        cupboardId,
+        recipeId
+      }});
+    res.json({ recordsDestroyed });
+  })
+);
 
 module.exports = router;
