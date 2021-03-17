7e4f92659a450f066fd2f664f24d7405e6b6087d
Daniel Upchurch <danielupchurch93@gmail.com>
2021-03-17 12:19:07 -0400

    added down to delete-recipe-ratings-table migration file in order to be able to run db:migrate:undo:all without error

--- db/migrations/20210107222626-create-delete-recipe-ratings-table.js
+++ db/migrations/20210107222626-create-delete-recipe-ratings-table.js
@@ -3,5 +3,8 @@ module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.dropTable('Recipe_Ratings"', {
     });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Recipe_Ratings');
   }
 };
